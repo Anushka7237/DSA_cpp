bfb065b070af0761ce9ffe29110593cfbf27dd7a
Anushka Gupta <anushkag327@gmail.com>
2024-12-28 13:35:12 +0530

    Update Reverse_a_string_with_spaces_intact.cpp

--- DSA/Reverse_a_string_with_spaces_intact.cpp
+++ DSA/Reverse_a_string_with_spaces_intact.cpp
@@ -1,31 +1,19 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
-int main()
-{
+int main() {
     string s = "Help others";
-    vector<int> vec;
-    int i = 0;
-    string str;
-    while (i <= s.length())
-    {
-        if (s[i] == ' ')
-            vec.push_back(i);
-        i++;
-    }
     int left = 0, right = s.length() - 1;
-    while (left < right)
-    {
-        if (s[left] == ' ')
-        {
+    while (left < right) {
+        // Skip spaces
+        if (s[left] == ' ') {
             left++;
-        }
-        else if (s[right] == ' ')
-        {
+        } else if (s[right] == ' ') {
             right--;
-        }
-        else{
+        } else {
+            // Swap characters
             swap(s[left], s[right]);
             left++;
             right--;
